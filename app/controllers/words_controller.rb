38d09871cf312e0de5925b2106148c33c091e71c
lady3bean <annehawleybrett@gmail.com>
2015-04-06 17:18:37 -0400

    controllers for index and show actions of words and word roots

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,15 +2,19 @@ class WordsController < ApplicationController
 
   def index
     @words = Word.all
+
+    render json: @words, status: 200, include: ['word_roots']
   end
 
   def show
     @word = Word.find(params[:id])
+
+    render json: @word, status: 200, include: ['word_roots']
   end
 
   private
   def word_params
-    word_params.require(:word).permit(:spelling, :definition)
+    word_params.require(:word).permit(:spelling, :definition, :word_roots)
   end
   
 end
