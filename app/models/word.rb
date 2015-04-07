b51d1dba28332fdc840a8dc83c73d5c1e726a89f
lady3bean <annehawleybrett@gmail.com>
2015-04-07 19:24:20 -0400

    testing and removing polymorphic: true statement in word_root model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,6 @@
 class Word < ActiveRecord::Base
   has_many :word_roots
-  has_many :roots, :through => :word_roots, :source => :root
+  has_many :roots, :through => :word_roots
 
   has_many :word_rooteds, :class_name => 'WordRoot', :foreign_key => 'root_id'
   has_many :rooteds, :through => :word_rooteds, :source => :word
