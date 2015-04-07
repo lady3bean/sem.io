354edd72088133f1323949aa346482e6a2c74538
lady3bean <annehawleybrett@gmail.com>
2015-04-07 17:52:12 -0400

    explains in comments and in conceptual naming of the data why this setup can't produce desired functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Word.delete_all
 WordRoot.delete_all
 
-parent = Word.create!(spelling: 'leaf', definition: 'nonsense')
-node = Word.create!(spelling: 'boombox', definition: 'sanity')
-child = Word.create!(spelling: 'ladeeda', definition: 'bon')
+grandparent = Word.create!(spelling: 'grandparent', definition: 'highest node in the family tree')
+parent = Word.create!(spelling: 'parent', definition: 'middle node with both roots and rooteds')
+child = Word.create!(spelling: 'child', definition: 'word with one root')
 
-wroot1 = WordRoot.create!(word: parent)
-wroot2 = WordRoot.create!(word: node)
+wroot1 = WordRoot.create!(word: grandparent)
+wroot2 = WordRoot.create!(word: parent)
 
-node.word_roots << wroot1
-child.word_roots << wroot2
+parent.word_roots << wroot1 #parent's word_root id should be 1, the grandparent's word_id 
+child.word_roots << wroot2 #similarly, child's word root id should be 2, the parent's word id. Instead, child.word_roots queries within word_roots for the word_id of child, and not parent. It's creating child as a wordroot, and referencing itself - which, looking at what I'm doing here, does make sense given this code, but it's not the functionality I desire.
 
+grandparent.save!
 parent.save!
-node.save!
 child.save!
-
-
+wroot1.save!
+wroot2.save!
