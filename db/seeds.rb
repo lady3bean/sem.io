b9330abab30a8ac4a3b2fd42919688cf9524804e
lady3bean <annehawleybrett@gmail.com>
2015-04-07 20:41:00 -0400

    modifies seeds file to reflect the dual nature of the model relationship, where you can assign and query by either word.rooteds or word.roots

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,9 @@ grandparent = Word.create!(spelling: 'grandparent', definition: 'highest node in
 parent = Word.create!(spelling: 'parent', definition: 'middle node with both roots and rooteds')
 child = Word.create!(spelling: 'child', definition: 'word with one root')
 
-wroot1 = WordRoot.create!(word: grandparent)
-wroot2 = WordRoot.create!(word: parent)
-
-parent.word_roots << wroot1 #parent's word_root id should be 1, the grandparent's word_id 
-child.word_roots << wroot2 #similarly, child's word root id should be 2, the parent's word id. Instead, child.word_roots queries within word_roots for the word_id of child, and not parent. It's creating child as a wordroot, and referencing itself - which, looking at what I'm doing here, does make sense given this code, but it's not the functionality I desire.
+grandparent.rooteds << parent
+child.roots << parent
 
 grandparent.save!
 parent.save!
 child.save!
-wroot1.save!
-wroot2.save!
