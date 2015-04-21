9765255e5a6da905efc0b9a1e437c9bb7c21558b
lady3bean <annehawleybrett@gmail.com>
2015-04-21 15:46:17 -0400

    index action for words queries by their spelling
    spelling has been indexed with btree for quicker searching

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,15 @@
 class WordsController < ApplicationController
 
+  def index
+    @words = Word.find_by spelling: params[:spelling]
+
+    if @words
+      render json: @words, status: 200, include: [:origins, :origin_ofs, :relations, :derivations, :derived_froms, :language]
+    else
+      render json: {message: 'no such word in my database. See contact info for how you can contribute to this project!'}, status: 404
+    end
+  end
+
   def show
     @word = Word.find(params[:id])
 
@@ -8,7 +18,7 @@ class WordsController < ApplicationController
 
   private
   def word_params
-    word_params.require(:word).permit(:spelling, :definition)
+    params.require(:word).permit(:spelling, :definition, :origins, :origin_ofs, :relations, :derivations, :derived_froms, :language)
   end
   
 end
